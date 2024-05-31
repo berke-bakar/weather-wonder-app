f60779f7ffce06f4abf8a1fd7a8dbd52fdf2d416
Berke Bakar <88384613+berke-bakar@users.noreply.github.com>
2024-05-31 18:30:05 +0300

    -> Added analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Layout } from '@/components/dom/Layout'
 import '@/global.css'
 import Head from './head'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function RootLayout({ children }) {
   return (
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
       <body>
         {/* To avoid FOUT with styled-components wrap Layout with StyledComponentsRegistry https://beta.nextjs.org/docs/styling/css-in-js#styled-components */}
         <Layout>{children}</Layout>
+        <Analytics />
       </body>
     </html>
   )
